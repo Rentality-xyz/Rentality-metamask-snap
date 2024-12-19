d66565faeb14c76764993f98a310bd062ffabf7a
danyloday <danil.vladyka1@gmail.com>
2024-12-20 00:49:36 +0200

    Added available cars

--- src/index.js
+++ src/index.js
@@ -3,10 +3,61 @@
  */
 import { ManageStateOperation } from '@metamask/snaps-sdk';
 import banner from '../images/banner.png';
+import { ethers } from 'ethers';
 
 import type { OnHomePageHandler, OnRpcRequestHandler } from "@metamask/snaps-sdk";
 import {Box, Heading, Text, Image, Link, Divider} from "@metamask/snaps-sdk/jsx";
 
+async function getAvailableCars() {
+    const contractAbi = [
+        {
+            "type": "function",
+            "name": "getAvailableCarsForUser",
+            "constant": true,
+            "stateMutability": "view",
+            "payable": false,
+            "inputs": [{"type": "address", "name": "user"}],
+            "outputs": [
+                {
+                    "type": "tuple[]",
+                    "name": "",
+                    "components": [
+                        {"type": "uint256", "name": "carId"},
+                        {"type": "string", "name": "carVinNumber"},
+                        {"type": "bytes32", "name": "carVinNumberHash"},
+                        {"type": "address", "name": "createdBy"},
+                        {"type": "string", "name": "brand"},
+                        {"type": "string", "name": "model"},
+                        {"type": "uint32", "name": "yearOfProduction"},
+                        {"type": "uint64", "name": "pricePerDayInUsdCents"},
+                        {"type": "uint64", "name": "securityDepositPerTripInUsdCents"},
+                        {"type": "uint8", "name": "engineType"},
+                        {"type": "uint64[]", "name": "engineParams"},
+                        {"type": "uint64", "name": "milesIncludedPerDay"},
+                        {"type": "uint32", "name": "timeBufferBetweenTripsInSec"},
+                        {"type": "bool", "name": "currentlyListed"},
+                        {"type": "bool", "name": "geoVerified"},
+                        {"type": "string", "name": "timeZoneId"},
+                        {"type": "bool", "name": "insuranceIncluded"},
+                        {"type": "bytes32", "name": "locationHash"}
+                    ]
+                }
+            ]
+        }
+    ]
+    const contractAddress = "0xCf261b0275870d924d65d67beB9E88Ebd8deE693";
+    const rpcUrl = "https://mainnet.base.org";
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+    const contract = new ethers.Contract(contractAddress, contractAbi, provider);
+    let availableCars = 0
+    try {
+        const cars = await contract.getAvailableCarsForUser(contractAddress);
+        availableCars = cars.length
+    } catch (error) {
+        console.error("Error:", error);
+    }
+    return availableCars
+}
 
 export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
     return ""
@@ -14,22 +65,15 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
 
 export const onHomePage: OnHomePageHandler = async () => {
 
+    const cars = await getAvailableCars();
+
     return {
         content: (
             <Box>
                 <Image src={banner}/>
-                {/*<Text>{persistedData.message || JSON.stringify(persistedData)}</Text>*/}
-                <Box alignment={'space-between'}  direction="horizontal">
-                    <Heading>Balance</Heading>
-                    <Heading>0</Heading>
-                </Box>
                 <Box alignment={'space-between'}  direction="horizontal">
                     <Heading>Available cars</Heading>
-                    <Heading>37</Heading>
-                </Box>
-                <Box alignment={'space-between'}  direction="horizontal">
-                    <Heading>Is host</Heading>
-                    <Heading>❌</Heading>
+                    <Heading>{cars.toString()}</Heading>
                 </Box>
                 <Divider />
                 <Link href="https://app.rentality.xyz">Rent Car</Link>
